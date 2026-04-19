673376d469c487c5df6ba58559faf5328ef1e7c0
Jose Siles <joseblancosiles@gmail.com>
2026-04-19 20:11:02 +0200

    updting fct_orders

--- dbt_project/models/marts/fct_orders.sql
+++ dbt_project/models/marts/fct_orders.sql
@@ -2,6 +2,10 @@ with orders as (
     select * from {{ ref('stg_orders') }}
 ),
 
+customers as (
+    select * from {{ ref('stg_customers') }}
+),
+
 products as (
     select * from {{ ref('stg_products') }}
 ),
@@ -16,9 +20,14 @@ final as (
         o.UNIT_PRICE,
         round(o.QUANTITY * o.UNIT_PRICE, 2) as REVENUE,
         o.ORDER_STATUS,
+        c.NAME as CUSTOMER_NAME,
+        c.SEGMENT as CUSTOMER_SEGMENT,
+        c.COUNTRY_CODE,
+        p.PRODUCT_NAME,
         p.CATEGORY,
         p.BRAND_NAME
     from orders o
+    left join customers c on o.CUSTOMER_ID = c.CUSTOMER_ID
     left join products p on o.PRODUCT_ID = p.PRODUCT_ID
 )
 
